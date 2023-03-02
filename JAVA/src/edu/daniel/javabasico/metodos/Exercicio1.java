89cd8d4e228cac6e9628e2607dfec4f50489091a
Daniel-Assuncao89 <assuncao.daniel89@gmail.com>
2023-03-02 16:07:27 -0300

    Exercicio de Overload

--- JAVA/src/edu/daniel/javabasico/metodos/Exercicio1.java
+++ JAVA/src/edu/daniel/javabasico/metodos/Exercicio1.java
@@ -40,4 +40,24 @@ public class Exercicio1 {
 
         System.out.println("Valor do emprestimo:" + valor + "\nValor da taxa de juros: " + (valor * taxa));
     }
+
+    public static void area(double lado) {
+
+        System.out.println("Area do quadrado:" + lado * lado);
+    }
+
+    public static void area(double lado1, double lado2) {
+
+        System.out.println("Area do retângulo:" + lado1 * lado2);
+    }
+
+    public static void area(double baseMaior, double baseMenor, double altura) {
+
+        System.out.println("Area do trapézio:" + ((baseMaior+baseMenor)*altura) / 2);
+    }
+
+    public static void area(float diagonal1, float diagonal2) {
+
+        System.out.println("Area do losango:" + (diagonal1 * diagonal2)/2);
+    }
 }
--- JAVA/src/edu/daniel/javabasico/metodos/Main.java
+++ JAVA/src/edu/daniel/javabasico/metodos/Main.java
@@ -7,5 +7,10 @@ public class Main {
         testador.operacoesBasicas(4,2);
         testador.saudacao(14);
         testador.emprestimo(5000);
+
+        testador.area(5);
+        testador.area(5.0,5.0);
+        testador.area(3,4,5);
+        testador.area(2f,2f);
     }
 }
