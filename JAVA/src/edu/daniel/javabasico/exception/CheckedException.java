3e4d960a664ff4d3e022fbde00999e5f96d00c6e
Daniel-Assuncao89 <assuncao.daniel89@gmail.com>
2023-03-06 16:26:13 -0300

    Tratamento de exceção: Error, unchecked, checked e custom

---
+++ JAVA/src/edu/daniel/javabasico/exception/CheckedException.java
@@ -0,0 +1,40 @@
+package edu.daniel.javabasico.exception;
+
+import javax.swing.*;
+import java.io.*;
+
+//Imprimir arquivo no console.
+public class CheckedException {
+    public static void main(String[] args) {
+        String nomeDoArquivo = "tratamentoEdxcecao.txt";
+        try {
+            imprimirArquivoNoConsole(nomeDoArquivo);
+        } catch (FileNotFoundException e) {
+            JOptionPane.showMessageDialog(null, "Revise o nome do arquivo que deseja imprimir!" + e.getCause());
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "Ocorreu um erro inesperado!, entre em contato com o suporte!" + e.getCause());
+            e.printStackTrace();
+        } finally {
+            System.out.println("Chegou no finally");
+        }
+
+        System.out.println("Apesar da exception ou não, o programa continua...");
+    }
+
+    public static void imprimirArquivoNoConsole(String nomeDoArquivo) throws IOException {
+        File file = new File(nomeDoArquivo);
+
+        BufferedReader br = new BufferedReader(new FileReader(file.getName()));
+        String line = br.readLine(); //lê linha por linha q ta contida no bufferedReader
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        do {
+            bw.write(line);
+            bw.newLine();
+            line = br.readLine();
+        } while (line != null);
+        bw.flush();
+        br.close();
+    }
+}
---
+++ JAVA/src/edu/daniel/javabasico/exception/UncheckedException.java
@@ -0,0 +1,37 @@
+package edu.daniel.javabasico.exception;
+
+
+import javax.swing.*;
+
+// Fazer a divisão de 2 valores inteiros
+public class UncheckedException {
+    public static void main(String[] args) {
+
+        boolean continueLooping = true;
+        do {
+            String a = JOptionPane.showInputDialog("Numerador: ");
+            String b = JOptionPane.showInputDialog("Denominador: ");
+
+            try {
+                int resultado = dividir(Integer.parseInt(a), Integer.parseInt(b));
+                System.out.println("Resultado: " + resultado);
+                continueLooping = false;
+            } catch (NumberFormatException e) { //Exception q foi mostrada no terminal antes de colocar no try, catch
+                e.printStackTrace();
+                JOptionPane.showMessageDialog(null, "Entrada invalida, informe um numero inteiro!");
+            } catch (ArithmeticException e) {
+                e.printStackTrace();
+                JOptionPane.showMessageDialog(null, "Impossivel dividir por zero.");
+            } finally {
+                System.out.println("Chegou no finally!");
+            }
+        } while(continueLooping);
+
+
+        System.out.println("O codigo continua...");
+    }
+
+    public static int dividir(int a, int b) {
+        return a / b;
+    }
+}
